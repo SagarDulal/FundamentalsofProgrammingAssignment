7e8571c0d1391b5105d87ead01c8c02cf7350957
Sagar Dulal <sagardulal100@gmail.com>
2023-09-10 17:45:27 +0545

    Student marks displayed in order

--- StudentMarksStatistics.java
+++ StudentMarksStatistics.java
@@ -4,10 +4,15 @@ public class StudentMarksStatistics
 {
 
     public static void main(String[] args){
+        
+        
+        
         // F1: Allows the user to input the assessment name
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the assignment name");
         String assessmentName = input.nextLine();
+        
+        
         
         
         //F2: Add student marks for 30 students.
@@ -32,12 +37,21 @@ public class StudentMarksStatistics
             } while (mark < 0 || mark > 30);
             marks[i] = mark;
         }
+        
+        
+        
         //F4: Prints out the assessment marks with the name of the assignment. 
         System.out.println("Marks of 30 students for the assessment of "+assessmentName);
         // For displaying the marks inputted by the user
         for(int i=0; i<marks.length; i++){
-            System.out.print(marks[i]+ "     ");
+            System.out.printf("Student %2d: %.2f\n", (i + 1), marks[i]);
+
         }
+        
+        
+        
+        
+        
     
     }
     
