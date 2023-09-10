7ce74b1a7b4daebb438974de4476b76c38315d87
Sagar Dulal <sagardulal100@gmail.com>
2023-09-10 16:05:17 +0545

    First function added

---
+++ StudentMarksStatistics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StudentMarksStatistics
+{
+
+    public static void main(String[] args){
+        // F1: Allows the user to input the assessment name
+        Scanner input = new Scanner(System.in);
+        String assessmentName = input.nextLine();
+    }
+    
+}
